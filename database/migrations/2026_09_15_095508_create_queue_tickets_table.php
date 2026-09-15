db25482d8c2d09f575b97a236ee1136c02c9e3a2
Stuwart <peppafa111@gmail.com>
2026-09-15 21:45:18 +0800

    chore: update database Migration

--- database/migrations/2026_09_15_095508_create_queue_tickets_table.php
+++ database/migrations/2026_09_15_095508_create_queue_tickets_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('queue_tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('tracking_number')->unique();
+            $table->uuid('device_id');
+            $table->string('mobile_number')->nullable();
+            $table->string('status')->default('holding');
+            $table->string('assigned_teller')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('queue_tickets');
