be97e7afc27051cf02db435f9ffb76637e6b69c1
Stuwart <peppafa111@gmail.com>
2026-09-17 00:30:37 +0800

    refractor: transfering the code to controller so it be easy to read and neat

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserControllers;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::post('/submit-form', function (Request $request) {
-    $validatedData = $request->validate([
-        'name' => 'required|string|max:255',
-    ]);
-    return response()->json(['message' => 'Form submitted successfully!', 'data' => $validatedData]);
 
-    return redirect('/')->with('success', 'Form submitted successfully!');
-})->name('submit.form');
+Route::post('/submit', [UserControllers::class ,'store'])->name('submit.form');
+
+
+
+// Route::post('/submit-form', function (Request $request) {
+//     $validatedData = $request->validate([
+//         'name' => 'required|string|max:255',
+//     ]);
+//     return response()->json(['message' => 'Form submitted successfully!', 'data' => $validatedData]);
+
+//     return redirect('/')->with('success', 'Form submitted successfully!');
+// })->name('submit.form');
