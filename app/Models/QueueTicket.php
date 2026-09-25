66d2dbe694b2d5d4635cfbb3b37e99591b12ba53
Stuwart <peppafa111@gmail.com>
2026-09-25 19:48:43 +0800

    feat:adding new controller and changing some fields

--- app/Models/QueueTicket.php
+++ app/Models/QueueTicket.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class QueueTicket extends Model
 {
-    protected $table = 'queue_tickets';
+    protected $table = 'queue_tickets1';
 
     protected $fillable = [
         'name',
